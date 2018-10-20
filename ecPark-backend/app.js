9064ab3437fe945945a7120400cc22b43f9340ff
Samuel Gedaly Guenoun <samuelgedaly@MacBook-Pro-de-Gisela.local>
2018-10-20 14:18:57 -0400

    first commit

---
+++ ecPark-backend/app.js
@@ -0,0 +1,60 @@
+//require('dotenv').load();
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+//const config = require('./config/database');
+//const mongoose = require('mongoose');
+//var admin = require("firebase-admin");
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/testConnection', function(req, res) {
+  console.log('hello');
+  res.send(
+    {
+        message: 'Hello from server!',
+    });
+});
+
+/*
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+ //Check connection
+db.once('open', () => {
+  console.log('Connected to MongoDB!');
+});
+
+//Check for DB errors
+db.on('error', (err) => {
+  console.log(err);
+});
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Route Files
+const posts = require('./routes/posts');
+app.use('/posts', posts);
+*/
+
+app.listen(port, function(){
+  console.log('Server started!');
+});
---
+++ ecPark-client/App.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Image,
+Platform,
+ScrollView,
+StyleSheet,
+Text,
+TouchableOpacity,
+View,
+NavigatorIOS, } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      isLoading: true,
+      page: '1'
+    }
+  }
+
+  render() {
+    if (this.state.page === '1') {
+      return (
+        <View style={styles.container}>
+          <View style={styles.helpContainer}>
+            <Text style={styles.helpLinkText}>The Saving Game</Text>
+          </View>
+          <View style={styles.helpContainer}>
+            <TouchableOpacity onPress={this.testAPI} style={styles.helpLink}>
+              <Text style={styles.helpLinkText}>Press to talk to backend server</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      );
+    } else if (this.state.page === '2'){
+      return (
+        <View style={styles.container}>
+          <View style={styles.helpContainer}>
+            <Text style={styles.helpLinkText}>{this.state.backendMsg}</Text>
+          </View>
+          <View style={styles.helpContainer}>
+            <TouchableOpacity onPress={this.goBack} style={styles.helpLink}>
+              <Text style={styles.helpLinkText}>Back</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    }
+  }
+  testAPI = () => {
+    fetch("http://10.136.21.176:8000/testConnection") //put your machine's IPv4 address here (System Preferences>>Network>>TCP/IP)
+    .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          backendMsg: responseJson.message,
+          page: '2',
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+
+  goBack = () => {
+    this.setState({
+      page: '1',
+    });
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  outerContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+    marginTop: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
